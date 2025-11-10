2622088d5a6ca6d16ced5600cbb44f8c0bc0eec4
ingiec-msu <charl112@msu.edu>
2025-11-10 03:04:49 +0000

    Updated eleventy.js

--- project2/.eleventy.js
+++ project2/.eleventy.js
@@ -1,15 +1,8 @@
 module.exports = function(eleventyConfig) {
-
-  eleventyConfig.addPassthroughCopy("styles");
-
-  // Copy favicon if you decide to add one
-  eleventyConfig.addPassthroughCopy("favicon.ico");
-
   return {
     dir: {
-      input: ".",          // Root directory contains content
-      includes: "_includes", // Templates/layouts folder
-      output: "_site"        // Final site output
+      input: "project2",
+      includes: "_includes"
     }
   };
 };
